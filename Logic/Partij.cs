567e40f6f6c7b5285efd698cf5cb1f21d611433b
Piet2001 <piet2001@fireman.net>
2021-01-13 09:54:52 +0100

    Create Partij.cs

---
+++ Logic/Partij.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Logic
+{
+    public class Partij
+    {
+        public string Orde { get; }
+        public string Naam { get; }
+        public string Lijsttrekker { get; }
+
+        public Partij(string orde, string naam, string lijsttrekker)
+        {
+            Orde = orde;
+            Naam = naam;
+            Lijsttrekker = lijsttrekker;
+        }
+    }
+}
